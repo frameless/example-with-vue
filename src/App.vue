d9cdbfdb4b7aaf95b2a5cad0548244a9744cadbf
Ali Amori Kadhim <ali@frameless.io>
2025-02-18 12:06:23 +0100

    integrate design systeem & add lint

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { Heading, Button, HtmlContent } from '@utrecht/component-library-vue'
+import {
+  Button,
+  HtmlContent,
+  Heading2,
+  Heading3,
+  Paragraph,
+  Link,
+  Strong,
+} from '@utrecht/component-library-vue';
 import { contentData } from './data.ts';
 
 const mappedCategories = {
@@ -14,9 +22,8 @@ const mappedCategories = {
   uitersteTermijn: 'termijn',
   vereisten: 'voorwaarden',
   wtdBijGeenReactie: 'Wat te doen bij geen reactie',
-  deskMemo: 'KCC'
-}
-
+  deskMemo: 'KCC',
+};
 
 // Set "tekst" as the default selected category
 const selectedCategory = ref<string>('tekst');
@@ -26,64 +33,123 @@ const selectCategory = (category: string) => {
     selectedCategory.value = category;
   }
 };
-
 </script>
 
 <template>
-  <div class="utrecht-theme utrecht-document utrecht-page">
-    <header>
-    <Heading :level="1">Overige Objecten API Met NL design systeem</Heading>
-  </header>
-  <div class="container">
-    <aside class="sidebar">
-      <Button
-        v-for="(_content, key) in contentData"
-        :key="key"
-        @click="selectCategory(key)"
-        :appearance="selectedCategory === key ? 'secondary-action-button' : 'primary-action-button'"
-      >
-        {{ mappedCategories[key] }}
-      </Button>
-    </aside>
-
-    <!-- Main Content Area -->
-    <main class="content">
-      <section v-if="selectedCategory">
-        <HtmlContent v-html="contentData[selectedCategory as keyof typeof contentData]" />
-      </section>
-    </main>
+  <div class="utrecht-page-layout">
+    <div class="utrecht-document utrecht-page">
+      <utrecht-page-header>
+        <div class="utrecht-logo">
+          <Link href="#"> <utrecht-logo-image></utrecht-logo-image></Link>
+        </div>
+      </utrecht-page-header>
+      <div class="container">
+        <aside class="sidebar">
+          <Button
+            v-for="(_content, key) in contentData"
+            :key="key"
+            :appearance="
+              selectedCategory === key ? 'secondary-action-button' : 'primary-action-button'
+            "
+            @click="selectCategory(key)"
+          >
+            {{ mappedCategories[key] }}
+          </Button>
+        </aside>
+        <main class="content">
+          <article class="utrecht-article">
+            <section v-if="selectedCategory">
+              <HtmlContent v-html="contentData[selectedCategory as keyof typeof contentData]" />
+            </section>
+          </article>
+        </main>
+      </div>
+    </div>
+    <utrecht-page-footer>
+      <Heading2 className="utrecht-heading-2--reset-h2">Gemeente Utrecht</Heading2>
+      <div class="grid-container">
+        <section>
+          <Heading3 className="utrecht-heading-3--distanced">Telefoon</Heading3>
+          <Paragraph className="utrecht-paragraph--distanced">
+            <a
+              href="tel:+31302860000"
+              className="utrecht-link utrecht-link--html-a utrecht-link--telephone"
+            >
+              14 030
+            </a>
+          </Paragraph>
+          <ul role="list" class="utrecht-link-list utrecht-link-list--html-ul">
+            <li class="utrecht-link-list__item">
+              <a
+                href="https://www.utrecht.nl/contact/verkort-telefoonnummer-gemeente"
+                class="utrecht-link utrecht-link--html-a utrecht-link-list__link"
+              >
+                <utrecht-icon-chevron-right></utrecht-icon-chevron-right>
+                <span class="utrecht-link-list__link-text"> Hoe werkt 14 030? </span>
+              </a>
+            </li>
+            <li class="utrecht-link-list__item">
+              <a
+                href="https://www.utrecht.nl/contact"
+                class="utrecht-link utrecht-link--html-a utrecht-link-list__link"
+              >
+                <utrecht-icon-chevron-right></utrecht-icon-chevron-right>
+                <span class="utrecht-link-list__link-text">Meer contactinformatie</span>
+              </a>
+            </li>
+            <li class="utrecht-link-list__item">
+              <Link
+                href="https://www.utrecht.nl/over-deze-website"
+                class="utrecht-link utrecht-link--html-a utrecht-link-list__link"
+              >
+                <utrecht-icon-chevron-right></utrecht-icon-chevron-right>
+                <span class="utrecht-link-list__link-text">Over deze website</span>
+              </Link>
+            </li>
+          </ul>
+        </section>
+        <address
+          className="utrecht-page-footer__address utrecht-page-footer__address--reset-address"
+        >
+          <Heading3 className="utrecht-heading-3--distanced">Adres</Heading3>
+          <Paragraph className="utrecht-paragraph--distanced">
+            <Strong>Stadskantoor</Strong>
+            <br />
+            Stadsplateau 1<br />
+            3521AZ
+          </Paragraph>
+        </address>
+      </div>
+    </utrecht-page-footer>
   </div>
-</div>
 </template>
 
 <style scoped>
-
 .container {
   display: flex;
-  block-size: 100vh;
-  inline-size: 100vw;
 }
 
-header {
-padding-inline: 2rem;
-padding-block: 2rem;
-text-align: center;
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 1rem;
 }
 
 .sidebar {
   inline-size: 600px;
   padding-inline: 20px;
   padding-block: 20px;
-  background: #f4f4f4;
   display: flex;
   flex-direction: column;
   gap: 10px;
+  background-color: #fff;
 }
 
 .content {
   flex-grow: 1;
   padding: 20px;
   inline-size: 100dvw;
+  background-color: #fff;
 }
 </style>
 
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import '@utrecht/design-tokens/dist/index.css';
+import '@utrecht/body-css/dist/index.css';
+import '@utrecht/root-css/dist/index.css';
+import '@utrecht/logo-css/dist/index.css';
+import '@utrecht/surface-css/dist/index.css';
+import '@utrecht/link-list-css/dist/index.css';
+import './style.css';
+import { UtrechtComponentLibrary } from '@utrecht/web-component-library-vue';
 
-createApp(App).mount('#app')
+createApp(App).use(UtrechtComponentLibrary).mount('#app');
